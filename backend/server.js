d80b75c1fcc4862d0b797396e98231e0a07fb3e4
pcctonyt <104700781+pcctonyt@users.noreply.github.com>
2022-09-05 12:31:52 -0500

    9/5/2022 MERN btrav with reg/login

--- backend/server.js
+++ backend/server.js
@@ -21,4 +21,4 @@ app.use(errorHandler)
 app.listen(port, () => console.log(`Server started on port ${port}`))
 
 
-/* MongoDB's oSQL documents-database has collections, which are like tables in a relational database -- 1ST AND SECOND VIDEOS NOW BOTH DONE -- (25:47 second video is where the jwt token stuff starts, and 28:32 is where he starts testing the token coding in Postman) new git repo set up with the backupPostman.txt file, to prevent having to restart from the beginning again if further problems develop*/
+/* MongoDB's oSQL documents-database has collections, which are like tables in a relational database -- 1ST AND SECOND VIDEOS NOW BOTH DONE -- (25:47 second video is where the jwt token stuff starts, and 28:32 is where he starts testing the token coding in Postman) new git repo set up with the backupPostman.txt file, to prevent having to restart from the beginning again if further problems develop.  Start 3rd video 9/5/2022 25:12 out of 1:13:25 done Learn the MERN stack Fronted Authentication 3rd video*/
---
+++ frontend/src/App.js
@@ -0,0 +1,31 @@
+//in React Router v 6, nothing in the Routes tag except each route
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+    
+  );
+}
+
+export default App;
+
+// We'll have three pages
+//1. Dashboard
+//2. Login for already registered users
+//3. Register a new user
